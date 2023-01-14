831d83899783c1c164490cf85b6ef34a07ea6df3
Thomas Fouquert <tfouquert@gmail.com>
2023-01-14 20:10:34 +0100

    update

--- binary/sumOfTwo.js
+++ binary/sumOfTwo.js
@@ -2,11 +2,25 @@
 //  return the sum of the two integers
 // without using the operators + and -.
 
-let a = 1;
-let b = 2;
-let output = 3;
+// &	AND	Sets each bit to 1 if both bits are 1
+// |	OR	Sets each bit to 1 if one of two bits is 1
+// ^	XOR	Sets each bit to 1 if only one of two bits is 1
+// ~	NOT	Inverts all the bits
+// <<	Zero fill left shift	Shifts left by pushing zeros in from the right and let the leftmost bits fall off
+// >>	Signed right shift	Shifts right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+// >>>	Zero fill right shift	Shifts right by pushing zeros in from the left, and let the rightmost bits fall off
+
+const a = 1;
+const b = 2;
+const output = 3;
 
 const getSum = (a, b) => (b ? getSum(a ^ b, (a & b) << 1) : a);
+
+// const getSum = (a, b) => {
+//   const firstArr = new Array(a).fill(true);
+//   const secondArr = new Array(b).fill(true);
+//   return firstArr.concat(secondArr).length
+// }
 
 function sumOfTwo(a, b) {
   if (b == 0) {
@@ -15,3 +29,5 @@ function sumOfTwo(a, b) {
     return getSum(a ^ b, (a & b) << 1);
   }
 }
+
+console.log(sumOfTwo(a, b));
